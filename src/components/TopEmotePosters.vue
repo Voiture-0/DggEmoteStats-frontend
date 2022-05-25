64d81910e06397b8c019d0980dfcdb337eabdae3
Voiture-0 <TheRealVoiture@gmail.com>
2022-05-25 08:08:51 -0400

    Added basic example of watching emote name changes

---
+++ src/components/TopEmotePosters.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps, watchEffect } from 'vue'
+
+const props = defineProps<{
+  emoteName: string
+}>()
+
+watchEffect(() => {
+  loadData(props.emoteName)
+})
+
+function loadData(emoteName: string) {
+  console.log(`loading data for '${emoteName}'`)
+}
+</script>
+
+<template>
+  <div>{{ emoteName }}</div>
+</template>
+
+<style scoped></style>
--- src/views/EmoteView.vue
+++ src/views/EmoteView.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-import { useRoute } from 'vue-router'
+import TopEmotePosters from '@/components/TopEmotePosters.vue'
+import { ref, watchEffect } from 'vue'
+import { useRoute, RouterLink } from 'vue-router'
 
 const route = useRoute()
-const emoteId = parseInt(route.params.id as string)
+
+const emoteName = ref(route.params.id as string)
+
+watchEffect(() => {
+  emoteName.value = route.params.id as string
+})
 </script>
 
 <template>
   <main>
-    {{ $route.params.id }}
-    {{ emoteId }}
+    <RouterLink to="/emote/LULW">/emote/LULW</RouterLink>
+    <RouterLink to="/emote/MMMM">/emote/MMMM</RouterLink>
+    <RouterLink to="/emote/Wowee">/emote/Wowee</RouterLink>
+    <RouterLink to="/emote/PepoTurkey">/emote/PepoTurkey</RouterLink>
+
+    {{ emoteName }}
+
+    <TopEmotePosters :emoteName="emoteName" />
   </main>
 </template>
